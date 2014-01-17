4ca910536d55c90e015ed349531d8b8ca13aef6a
eileencodes <eileencodes@gmail.com>
2014-01-16 20:38:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigRubyConf2014::Application.config.secret_key_base = 'cc3e742f84f7f9d932b0e1a3c25ef2c3f9e94e2f9fb5c4f6f9eacb5fbcd82a9c094701b29aaf0a4f5c2bea404ba82e611181cb82dad1e89bd9ebce4b7625eb04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BigRubyConf2014::Application.config.session_store :cookie_store, key: '_big_ruby_conf_2014_session'
