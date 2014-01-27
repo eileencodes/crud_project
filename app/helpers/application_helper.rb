e17a43aac332b9aa2dc40ea85aa09ff2560a2409
eileencodes <eileencodes@gmail.com>
2014-01-27 08:47:16 -0500

    fix styles and set bootstrap classes
    Bootstrap form elements depend on classes. Need to extend
    bootstrap and the form elements to globally add these classes.
    I want to move this to it's own helper for forms later but
    this was a quick and dirty test to see if it worked.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,32 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  module BootstrapExtension
+    def text_field(name, value = nil, options = {})
+      class_name = options[:class]
+      options[:class] = "form-control"
+      super
+    end
+
+    def password_field(name, value = nil, options = {})
+      class_name = options[:class]
+      options[:class] = "form-control"
+      super
+    end
+
+    def text_field_tag(name, value = nil, options = {})
+      class_name = options[:class]
+      options[:class] = "form-control"
+      super
+    end
+
+    def submit_tag(name, options = {})
+      class_name = options[:class]
+      options[:class] = "btn btn-default"
+      super
+    end
+  end
+
+  include BootstrapExtension
 end
