800e7928d19cd5c8b2b4f947421682c18c0044f9
eileencodes <eileencodes@gmail.com>
2014-01-27 22:06:40 -0500

    add method to print full name of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :contacts
+
+  def full_name
+    if self.first_name && self.last_name
+      "#{self.first_name} #{self.last_name}"
+    elsif self.first_name
+      self.first_name
+    elsif self.last_name
+      self.last_name
+    else
+      self.email
+    end
+  end
 end
