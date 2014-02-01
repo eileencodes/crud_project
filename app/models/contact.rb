bf13815b02c2e468b5fc710617d3d462639afecf
eileencodes <eileencodes@gmail.com>
2014-02-01 17:12:53 -0500

    define fullname for contact model
    Different but similar to user model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,4 +5,16 @@ class Contact < ActiveRecord::Base
   def prefix_options
     [['Mr.', 1], ['Mrs.', 2], ['Ms.', 3], ['Miss', 4]]
   end
+
+  def full_name
+    if self.first_name && self.last_name
+      "#{self.first_name} #{self.last_name}"
+    elsif self.first_name
+      self.first_name
+    elsif self.last_name
+      self.last_name
+    else
+      "No Name"
+    end
+  end
 end
