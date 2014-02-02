eb343fec26a0df618ed3a1206f01af515e7de43d
eileencodes <eileencodes@gmail.com>
2014-02-02 11:57:56 -0500

    add helper methods to contact model for fullname, and addresses

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,18 +3,40 @@ class Contact < ActiveRecord::Base
   has_many :social_accounts
 
   def prefix_options
-    [['Mr.', 1], ['Mrs.', 2], ['Ms.', 3], ['Miss', 4]]
+    [['Mr.', 'Mr.'], ['Mrs.', 'Mrs.'], ['Ms.', 3], ['Miss', 4]]
   end
 
   def full_name
-    if self.first_name && self.last_name
-      "#{self.first_name} #{self.last_name}"
-    elsif self.first_name
-      self.first_name
-    elsif self.last_name
-      self.last_name
+    name = ""
+    name << "#{prefix} " if prefix.present?
+    if first_name && last_name
+      name << "#{first_name} #{last_name}"
+    elsif first_name
+      name << first_name
+    elsif last_name
+      name << last_name
     else
-      "No Name"
+      name
     end
+    name << " #{suffix}" if suffix.present?
+    name
+  end
+
+  def address
+    address = ""
+    address << address_1
+    address << "<br />#{address_2}" if address_2.present?
+    address << "<br />#{city}, #{state} #{postal_code}"
+    address << "<br />#{country}"
+    address.html_safe
+  end
+
+  def company_address
+    address = ""
+    address << company_address_1
+    address << "<br />#{company_address_2}" if company_address_2.present?
+    address << "<br />#{company_city}, #{company_state} #{company_postal_code}"
+    address << "<br />#{company_country}"
+    address.html_safe
   end
 end
