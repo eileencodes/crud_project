906f311ca47b708d48215212a6cf8a455cf43892
eileencodes <eileencodes@gmail.com>
2014-02-02 21:36:17 -0500

    add paging to contacts

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+gem 'kaminari'
 
 gem 'faker'
 
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   include ApplicationHelper
 
   def index
-    @contacts = current_user.contacts
+    @contacts = current_user.contacts.page(params[:page])
   end
 
   def new
