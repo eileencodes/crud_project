04aa24fe97aeb2db3e7a2619653f101818ee90d4
eileencodes <eileencodes@gmail.com>
2014-02-02 21:27:49 -0500

    add sample data creation/destroy/update/retrieve module
    This module will be used to demonstrate the issues with AR
    if we rely too much on the "magic". Still a work in progress
    though.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module BigRubyConf2014
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.autoload_paths << "#{Rails.root}/lib/sample_data"
   end
 end
