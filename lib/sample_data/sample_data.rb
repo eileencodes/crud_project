54eddad407d9569f2b6fb38dfa5a6578877aa175
eileencodes <eileencodes@gmail.com>
2014-02-08 17:28:06 -0500

    add retrieve examples and demonstration
    Retrieve has 3 examples. One slow one and two
    alternatives.

--- lib/sample_data/sample_data.rb
+++ lib/sample_data/sample_data.rb
@@ -50,9 +50,21 @@ module SampleData
   end
 
   def self.retrieve_contacts_oh_crud
+    Contact.where(:user_id => 1).each do |contact|
+      puts contact.first_name
+    end
   end
 
   def self.retrieve_contacts
+    Contact.where(:user_id => 1).find_each do |contact|
+      puts contact.first_name
+    end
+  end
+
+  def self.retrieve_contacts_alt
+    Contact.select(:first_name).each do |contact|
+      puts contact.first_name
+    end
   end
 
   def self.update_contacts_oh_crud
