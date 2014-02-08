b97760855c3937535187042dd615da3e322e3bd8
eileencodes <eileencodes@gmail.com>
2014-02-08 17:29:50 -0500

    make sure contacts include user_id
    I decided to make contacts owned by the user so they
    could be accessed through "all" contacts or through
    their assigned categories (on the category show page)

--- lib/sample_data/sample_data.rb
+++ lib/sample_data/sample_data.rb
@@ -28,7 +28,8 @@ module SampleData
         :company_city => csv[20],
         :company_state => csv[21],
         :company_postal_code => csv[22],
-        :company_country => csv[23]
+        :company_country => csv[23],
+        :user_id => csv[24]
       })
     end
   end
@@ -37,14 +38,14 @@ module SampleData
     contact_values = []
     categorization_values = []
     CSV.foreach("#{Rails.root}/lib/sample_data/contacts.csv", headers: true) do |csv|
-      contact_values << "('#{csv[0]}','#{csv[1]}','#{csv[2]}','#{csv[3]}','#{csv[4]}','#{csv[5]}','#{csv[6]}','#{csv[7]}','#{csv[8]}','#{csv[9]}','#{csv[10]}','#{csv[11]}','#{csv[12]}','#{csv[13]}','#{csv[14]}','#{csv[15]}','#{csv[16]}','#{csv[17]}','#{csv[18]}','#{csv[19]}','#{csv[20]}','#{csv[21]}','#{csv[22]}','#{csv[23]}')"
+      contact_values << "('#{csv[0]}','#{csv[1]}','#{csv[2]}','#{csv[3]}','#{csv[4]}','#{csv[5]}','#{csv[6]}','#{csv[7]}','#{csv[8]}','#{csv[9]}','#{csv[10]}','#{csv[11]}','#{csv[12]}','#{csv[13]}','#{csv[14]}','#{csv[15]}','#{csv[16]}','#{csv[17]}','#{csv[18]}','#{csv[19]}','#{csv[20]}','#{csv[21]}','#{csv[22]}','#{csv[23]}','#{csv[24]}')"
     end
 
     batch_size = 2000
 
     while !contact_values.empty?
       contacts_shifted = contact_values.shift(batch_size)
-      contacts_sql = "INSERT INTO contacts(first_name, last_name, birthday, phone, email, twitter_account_link, facebook_account_link, linkedin_account_link, gplus_account_link, github_account_link, address_1, address_2, city, state, postal_code, country, company, title, company_address_1, company_address_2, company_city, company_state, company_postal_code, company_country) VALUES#{contacts_shifted.join(", ")}"
+      contacts_sql = "INSERT INTO contacts(first_name, last_name, birthday, phone, email, twitter_account_link, facebook_account_link, linkedin_account_link, gplus_account_link, github_account_link, address_1, address_2, city, state, postal_code, country, company, title, company_address_1, company_address_2, company_city, company_state, company_postal_code, company_country, user_id) VALUES#{contacts_shifted.join(", ")}"
       ActiveRecord::Base.connection.execute(contacts_sql)
     end
   end
@@ -85,7 +86,7 @@ module SampleData
     Contact.where(:user_id => user_id).delete_all
   end
 
-  def self.create_contacts_csv(amount, filename)
+  def self.create_contacts_csv(amount, filename, user_id)
     CSV.open("lib/sample_data/#{filename}", "wb") do |csv|
       csv << [ 'First Name', 'Last Name', 'Birthday', 'Phone', 'Email', 'Twitter', 'Facebook', 'LinkedIn', 'Googple Plus', 'Github', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country', 'Company', 'Title', 'Company Address 1', 'Company Address 2', 'Company City', 'Company State', 'Company Postal Code', 'Company Country', 'User Id' ]
       amount.times do |n|
@@ -94,7 +95,7 @@ module SampleData
         email_address = "#{first_name.downcase}.#{last_name.downcase}.#{n}@example.com"
         profile_name = "#{first_name.downcase}_#{n}_sample"
         csv << [
-          first_name, last_name, "1987-02-01", Faker::PhoneNumber.phone_number, email_address, "http://www.twitter.com/#{profile_name}", "http://www.facebook.com/#{profile_name}", "http://www.linkedin.com/in/#{profile_name}", "http://gplus.google.com/posts/#{profile_name}", "http://www.github.com/#{profile_name}", Faker::Address.street_address.gsub("'", ""), Faker::Address.secondary_address.gsub("'", ""), Faker::Address.city.gsub("'", ""), Faker::Address.state_abbr, Faker::Address.zip, "USA", Faker::Company.name.gsub("'", ""), Faker::Name.title.gsub("'", ""), Faker::Address.street_address.gsub("'", ""),  Faker::Address.secondary_address.gsub("'", ""),  Faker::Address.city.gsub("'", ""), Faker::Address.state_abbr, Faker::Address.zip, "USA"
+          first_name, last_name, "1987-02-01", Faker::PhoneNumber.phone_number, email_address, "http://www.twitter.com/#{profile_name}", "http://www.facebook.com/#{profile_name}", "http://www.linkedin.com/in/#{profile_name}", "http://gplus.google.com/posts/#{profile_name}", "http://www.github.com/#{profile_name}", Faker::Address.street_address.gsub("'", ""), Faker::Address.secondary_address.gsub("'", ""), Faker::Address.city.gsub("'", ""), Faker::Address.state_abbr, Faker::Address.zip, "USA", Faker::Company.name.gsub("'", ""), Faker::Name.title.gsub("'", ""), Faker::Address.street_address.gsub("'", ""),  Faker::Address.secondary_address.gsub("'", ""),  Faker::Address.city.gsub("'", ""), Faker::Address.state_abbr, Faker::Address.zip, "USA", user_id
         ]
       end
     end
