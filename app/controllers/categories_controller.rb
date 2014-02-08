fbbc7787f0ee128c4138af539a4c9a992a179bd8
eileencodes <eileencodes@gmail.com>
2014-02-08 17:30:37 -0500

    add edit, update, new, create, and destroy methods to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,9 +5,51 @@ class CategoriesController < ApplicationController
     @categories = current_user.categories
   end
 
+  def new
+    @category = Category.new
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to user_categories_path(current_user.id), notice: "Category was successfully created." }
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
   def show
     @category = Category.find(params[:id])
     @contacts = @category.contacts.page(params[:page])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    respond_to do |format|
+      if @category.update_attributes(category_params)
+        format.html { redirect_to edit_user_category_path(current_user, @category), notice: "Category successfully updated." }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to user_categories_path(current_user)
   end
 
   private
