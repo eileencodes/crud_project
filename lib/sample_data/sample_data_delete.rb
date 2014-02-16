6bfbd0f8334fc8d29dd4ed82022b55f583513dde
eileencodes <eileencodes@gmail.com>
2014-02-16 12:41:16 -0500

    fix incorrect variable declaration in delete methods

--- lib/sample_data/sample_data_delete.rb
+++ lib/sample_data/sample_data_delete.rb
@@ -2,11 +2,11 @@ module SampleDataDelete
   # DO NOT run with large data sets (10k or more) unless you are ok
   # with killing your MySQL instance
   def self.delete_contacts_oh_crud(category)
-    cat.categorizations.destroy_all
+    category.categorizations.destroy_all
   end
 
   def self.delete_contacts_oh_crud_alt(category)
-    cat.categorizations.delete_all
+    category.categorizations.delete_all
   end
 
   def self.delete_contacts_optimized(category)
